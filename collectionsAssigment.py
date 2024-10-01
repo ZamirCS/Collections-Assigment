4501d50bc96ecf8117d1e7dfacfd3b15e222388c
Zamir <zamir357@icloud.com>
2024-09-30 20:52:51 -0400

    Collections Assigment

---
+++ collectionsAssigment.py
@@ -0,0 +1,98 @@
+name = input('Enter name\n')
+
+gradeList = [int(i) for i in input("Enter grades:\n").split()]
+
+def avarage():
+    gradeAvarge = sum(gradeList)/len(gradeList)
+    return gradeAvarge
+
+def letter():
+    if avarage() >= 90:
+        letterGrade = 'A'
+    elif avarage() >= 80:
+        letterGrade = 'B'
+    elif avarage() >= 70:
+        letterGrade = 'C'
+    elif avarage() >= 60:
+        letterGrade = 'D'
+    else:
+        letterGrade = 'F'
+    return letterGrade
+
+
+
+print(name)
+print('Average:',round(avarage(),1))
+print('Letter Grade:',letter())
+print()
+
+name = input('Enter name\n')
+
+gradeList = [int(i) for i in input("Enter grades:\n").split()]
+
+avarage()
+letter()
+
+print(name)
+print('Average:',round(avarage(),1))
+print('Letter Grade:',letter())
+print()
+
+name = input('Enter name\n')
+
+gradeList = [int(i) for i in input("Enter grades:\n").split()]
+
+avarage()
+letter()
+
+print(name)
+print('Average:',round(avarage(),1))
+print('Letter Grade:',letter())
+print()
+
+name = input('Enter name\n')
+
+gradeList = [int(i) for i in input("Enter grades:\n").split()]
+
+avarage()
+letter()
+
+print(name)
+print('Average:',round(avarage(),1))
+print('Letter Grade:',letter())
+print()
+
+name = input('Enter name\n')
+
+gradeList = [int(i) for i in input("Enter grades:\n").split()]
+
+avarage()
+letter()
+
+print(name)
+print('Average:',round(avarage(),1))
+print('Letter Grade:',letter())
+print()
+
+name = input('Enter name\n')
+
+gradeList = [int(i) for i in input("Enter grades:\n").split()]
+
+avarage()
+letter()
+
+print(name)
+print('Average:',round(avarage(),1))
+print('Letter Grade:',letter())
+print()
+
+name = input('Enter name\n')
+
+gradeList = [int(i) for i in input("Enter grades:\n").split()]
+
+avarage()
+letter()
+
+print(name)
+print('Average:',round(avarage(),1))
+print('Letter Grade:',letter())
